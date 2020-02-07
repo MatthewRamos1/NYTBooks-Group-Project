bba9baca52fb1d51bed417a65ae73ff1e96bd5dc
Matthew Ramos <mmramos309@gmail.com>
2020-02-07 16:36:45 -0500

    Created settings view

---
+++ NYTBooks-Group-Project/Views/SettingsView.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsView.swift
+//  NYTBooks-Group-Project
+//
+//  Created by Matthew Ramos on 2/7/20.
+//  Copyright © 2020 Matthew Ramos. All rights reserved.
+//
+
+import UIKit
+
+class SettingsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
