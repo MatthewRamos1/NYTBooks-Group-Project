7e9f90a1becfbee4f723a6493f571895544be85a
David <lin.david985@gmail.com>
2020-02-14 17:38:31 -0500

    added settings view

--- NYTBooks-Group-Project/Network Services/NYTAPIClient.swift
+++ NYTBooks-Group-Project/Network Services/NYTAPIClient.swift
@@ -11,7 +11,10 @@ import NetworkHelper
 
 struct NYTAPIClient {
   static func fetchBooks(for type: String, completion: @escaping (Result<[Books], AppError>) -> ()) {
-    let endpointString = "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=\(Config.apiKey)"
+    
+    let bookList = type.replacingOccurrences(of: " ", with: "-")
+    
+    let endpointString = "https://api.nytimes.com/svc/books/v3/lists/current/\(bookList).json?api-key=\(Config.apiKey)"
     guard let url = URL(string: endpointString) else {
       completion(.failure(.badURL(endpointString)))
       return
@@ -28,6 +31,32 @@ struct NYTAPIClient {
           let query = try JSONDecoder().decode(TopBooks.self, from: data)
           let books = query.results.books
           completion(.success(books))
+        } catch {
+          completion(.failure(.decodingError(error)))
+        }
+      }
+    }
+  }
+    
+  static func fetchBookTypes(completion: @escaping (Result<[BookTypes], AppError>) -> ()) {
+    
+    let endpointString = "https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=\(Config.apiKey)"
+    guard let url = URL(string: endpointString) else {
+      completion(.failure(.badURL(endpointString)))
+      return
+    }
+    
+    let request = URLRequest(url: url)
+    
+    NetworkHelper.shared.performDataTask(with: request) { (result) in
+      switch result {
+      case .failure(let appError):
+        completion(.failure(.networkClientError(appError)))
+      case .success(let data):
+        do {
+          let query = try JSONDecoder().decode(List.self, from: data)
+            let list = query.results
+          completion(.success(list))
         } catch {
           completion(.failure(.decodingError(error)))
         }
--- NYTBooks-Group-Project/Views/BestSellerView.swift
+++ NYTBooks-Group-Project/Views/BestSellerView.swift
@@ -9,58 +9,63 @@
 import UIKit
 
 class BestSellerView: UIView {
-  
-  public lazy var collectionView: UICollectionView = {
-    let layout = UICollectionViewFlowLayout()
-    layout.scrollDirection = .horizontal
-    layout.itemSize = CGSize(width: 300, height: 400)
-    let cv = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
-    cv.backgroundColor = .systemRed
-    return cv
-  }()
-  
-  public lazy var pickerView : UIPickerView = {
-    let pv = UIPickerView()
-    pv.backgroundColor = .blue
-    return pv
-  }()
     
-
-  override init(frame: CGRect) {
-    super.init(frame: UIScreen.main.bounds)
-    commonInit()
-  }
-  
-  required init?(coder: NSCoder) {
-    super.init(coder: coder)
-    commonInit()
-  }
-  
-  private func commonInit() {
-    collectionViewSetup()
-    pickerViewSetup()
-  }
-
-  private func collectionViewSetup() {
-    addSubview(collectionView)
-    collectionView.translatesAutoresizingMaskIntoConstraints = false
-    NSLayoutConstraint.activate([
-      collectionView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
-      collectionView.bottomAnchor.constraint(equalTo: centerYAnchor, constant: 75),
-      collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
-      collectionView.trailingAnchor.constraint(equalTo: trailingAnchor)
-    ])
-  }
-  
-  private func pickerViewSetup() {
-    addSubview(pickerView)
-    pickerView.translatesAutoresizingMaskIntoConstraints = false
-    NSLayoutConstraint.activate([
-      pickerView.topAnchor.constraint(equalTo: collectionView.bottomAnchor, constant: 0),
-      pickerView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: 0),
-      pickerView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0),
-      pickerView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0)
-    ])
-  }
-
+    public lazy var collectionView: UICollectionView = {
+        let collectionViewBackgroundView = UIView()
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame.size = frame.size
+        gradientLayer.colors = [#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1).cgColor,#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1).cgColor, #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).cgColor]
+        gradientLayer.shouldRasterize = true
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: 200, height: 300)
+        let cv = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        cv.backgroundView = collectionViewBackgroundView
+        cv.backgroundView?.layer.addSublayer(gradientLayer)
+        return cv
+    }()
+    
+    public lazy var pickerView : UIPickerView = {
+        let pv = UIPickerView()
+        pv.backgroundColor = .white
+        return pv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        collectionViewSetup()
+        pickerViewSetup()
+    }
+    
+    private func collectionViewSetup() {
+        addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: centerYAnchor, constant: 75),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+    }
+    
+    private func pickerViewSetup() {
+        addSubview(pickerView)
+        pickerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            pickerView.topAnchor.constraint(equalTo: collectionView.bottomAnchor, constant: 0),
+            pickerView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: 0),
+            pickerView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0),
+            pickerView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0)
+        ])
+    }
+    
 }
